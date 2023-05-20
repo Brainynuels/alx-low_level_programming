a04bb5baa6c4e724cf7edae780f83179094274e5
brainynuels <brainyimmanuel@gmail.com>
2023-05-20 00:52:09 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,8 +21,5 @@ int main(void)
 		printf("0; and is 0\n");
 	else
 		printf("-8; and is less than 6 and not 0\n");
-	else
-		printf("Last digit of %d is -8 and is less than 6 and not 0\n");
-
-	return (0);
+		return (0);
 }
