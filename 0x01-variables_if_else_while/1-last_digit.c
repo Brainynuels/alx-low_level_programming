559fd37e816f1908700bc749927cb7b5fbbe6bbf
brainynuels <brainyimmanuel@gmail.com>
2023-05-19 17:32:27 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,13 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is ", n);
+	printf("Last digit of %d is 8 ", n);
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
-		printf("and is 0\n");
+		printf("0 and is 0\n");
 	else
-		printf("and is less than 6 and not 0\n");
+		printf("-8 and is less than 6 and not 0\n");
 
 	return (0);
 }
