7b8324383847f35a21efa620ffd650bcb97977e3
brainynuels <brainyimmanuel@gmail.com>
2023-06-04 08:39:26 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,51 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - convert a string into an integer.
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
  *
- * @s: the string to use. 
- * Return: integer.
+ * Return: result of conversion
  */
-
 int _atoi(char *s)
 {
-	int sign = 1, i = 0;
-	unsigned int res = 0;
+	unsigned int n = 0;
+	int sign = 1;
+	int flag = 0;
 
-	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == '-')
+		if (*s >= '0' && *s <= '9')
+		{
+			flag = 1;
+			if (sign > 0)
+			{
+				if (INT_MAX / 10 < (int) n)
+					return (INT_MAX);
+				n *= 10;
+				if (INT_MAX - (*s - '0') < (int) n)
+					return (INT_MAX);
+			}
+			else
+			{
+				if (INT_MIN / 10 < (int) -n)
+					return (INT_MIN);
+				n *= 10;
+				if (INT_MIN + (*s - '0') > (int) -n)
+					return (INT_MIN);
+			}
+			n += (*s - '0');
+		}
+		else if (flag)
+		{
+			break;
+		}
+		else if (*s == '-')
+		{
 			sign *= -1;
-		i++;
+		}
+		++s;
 	}
-	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-	{
-		res = (res * 10) + (s[i] - '0');
-		i++;
-	}
-	res *= sign;
-	return (res);
+
+	return (n * sign);
 }
